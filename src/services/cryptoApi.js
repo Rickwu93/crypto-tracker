76c1c1fecfa74d9662b4c8effe9012fa9a5a6ca2
Ricky Wu <rickywu209@gmail.com>
2022-12-29 19:31:41 -0700

    adding in crypto api from rapidapi

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,20 @@
+
+
+
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/coins',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       timePeriod: '24h',
+//       'tiers[0]': '1',
+//       orderBy: 'marketCap',
+//       orderDirection: 'desc',
+//       limit: '50',
+//       offset: '0'
+//     },
+//     headers: {
+//       'X-RapidAPI-Key': '',
+//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+//   };
