5d6cf2feb695e29967233380ce37d15803166b3b
Ricky Wu <rickywu209@gmail.com>
2022-12-30 16:43:55 -0700

    storing our crypto api in redux object

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+//importing store will require provider from redux
+import { Provider } from 'react-redux';
 
 import App from './App';
+import store from './app/store';
 import 'antd/dist/reset.css';
 
 ReactDOM.render(
     <Router>
-        <App />
+        <Provider store={store}>
+            <App /> 
+        </Provider>
     </Router>, 
     document.getElementById('root')
 );
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,20 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
 
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': '',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
 
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+//for the query calls to the url
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
-// const options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/coins',
-//     params: {
-//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//       timePeriod: '24h',
-//       'tiers[0]': '1',
-//       orderBy: 'marketCap',
-//       orderDirection: 'desc',
-//       limit: '50',
-//       offset: '0'
-//     },
-//     headers: {
-//       'X-RapidAPI-Key': '',
-//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-//     }
-//   };
+//passing createApi from redux in an object
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi', //says what this reducer is for
+    //pass in function that accept an object, our api url
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    //lastly endpoints where you pass in builder function that returns an object
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/exchanges')
+        })
+    })
+});
